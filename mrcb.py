927873779d94f901199d80b296b85d431c558d2d
Georgi D. Sotirov <gdsotirov@gmail.com>
2020-02-23 16:41:07 +0200

    Remove message types, use pinfos instead of pinfoc, use pinfoe for status

--- mrcb.py
+++ mrcb.py
@@ -35,41 +35,41 @@ def main():
   try:
     cfg_file = open(MRCB_CONFIG, "r")
   except Exception as err:
-    e.perror("Error: Cannot open configuration file '%s': %s" % (MRCB_CONFIG, str(err)))
+    e.perror("Cannot open configuration file '%s': %s" % (MRCB_CONFIG, str(err)))
     return 1
 
   # Load configuration
   try:
     cfg = json.load(cfg_file)
   except Exception as err:
-    e.perror("Error: Cannot read configuration: %s" % str(err))
+    e.perror("Cannot read configuration: %s" % str(err))
     return 2
 
   cfg_file.close()
 
   if not cfg.get('backup_dir'):
-    e.pwarn("Warning: Using default backup directory '%s'. Please, check your configuration." % DEF_BACKUPDIR)
+    e.pwarn("Using default backup directory '%s'. Please, check your configuration." % DEF_BACKUPDIR)
     cfg['backup_dir'] = DEF_BACKUPDIR
 
   # Check if backup directory exist and try to create it
   if not os.path.exists(cfg['backup_dir']):
     try:
-      e.pinfo("Info: Creating directory '%s'." % cfg['backup_dir'])
+      e.pinfo("Creating directory '%s'." % cfg['backup_dir'])
       os.mkdir(cfg['backup_dir'])
     except Exception as err:
-      e.perror("Error: Cannot create backup directory '%s': %s" % (cfg['backup_dir'], str(err)))
+      e.perror("Cannot create backup directory '%s': %s" % (cfg['backup_dir'], str(err)))
       return 3
   elif not os.path.isdir(cfg['backup_dir']):
-    e.perror("Error: Path '%s' set as 'backup_dir' is not a directory!" % cfg['backup_dir'])
+    e.perror("Path '%s' set as 'backup_dir' is not a directory!" % cfg['backup_dir'])
     return 4
 
   if not cfg.get('routers'):
-    e.perror("Error: No routers configured! Please, fix your configuration.")
+    e.perror("No routers configured! Please, fix your configuration.")
     return 5
 
   # Loop routers and dump configuration
   for rtr in cfg['routers']:
-    e.pinfoc("Info: Backing up configuration of '%s'... " % rtr['name'])
+    e.pinfos("Backing up configuration of '%s'... " % rtr['name'])
 
     try:
       ros = routeros.SecureTransport(rtr['hostname'], rtr['port'])
@@ -82,11 +82,11 @@ def main():
       ros.get_export(local_exp_file)
       ros.close()
     except Exception as err:
-      e.pinfo("Fail.")
-      e.perror("Error: Cannot get configuration: %s" % str(err))
+      e.pinfoe("Fail.")
+      e.perror("Cannot get configuration: %s" % str(err))
       continue
 
-    e.pinfo("Done.")
+    e.pinfoe("Done.")
 
 exit(main())
 
